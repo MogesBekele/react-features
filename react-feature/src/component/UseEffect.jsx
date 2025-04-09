7725dfd44dcc5d40546a4b031751f6769ae76c35
MogesBekele <mogesbekele32@gmail.com>
2025-04-09 14:17:56 +0300

    create useeffect component

--- react-feature/src/component/UseEffect.jsx
+++ react-feature/src/component/UseEffect.jsx
@@ -25,7 +25,7 @@ const UseEffect = () => {
 
 
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800'>
 
       <h1 className='text-4xl font-bold'>UseEffect</h1>
       <p className='text-lg mt-2'>The following is a simple useEffect example using React and Tailwind CSS</p>
