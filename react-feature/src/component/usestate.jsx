56b5453eeecd9e66bbdaeb53b3826d3b4244031b
MogesBekele <mogesbekele32@gmail.com>
2025-04-09 14:01:26 +0300

    create component

---
+++ react-feature/src/component/usestate.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+
+const usestate = () => {
+  const [count, setCount] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);// toogle the browser
+  };
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setSubmittedName(name);
+    setName("");
+  }
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-center min-h-screen ${
+        isDarkMode ? "bg-gray-800 text-white" : "bg-gray-100 text-gray-800"
+      }`}
+    >
+      <h1 className="text-4xl font-bold">Counter App</h1>
+
+      <p className="text-lg mt-2">
+        The following is a simple counter app using React and Tailwind CSS
+      </p>
+
+      <p className="text-lg text-green-400">Current count: {count}</p>
+      <div className="flex gap-4 mt-4">
+        <button
+          className="bg-amber-700 text-white rounded-full px-6 py-2"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Increment
+        </button>
+
+        <button
+          className="bg-amber-700 text-white rounded-full px-6 py-2"
+          onClick={() => setCount((count) => count - 1)}
+        >
+          Decrement
+        </button>
+        <button
+          className="bg-amber-700 text-white rounded-full px-6 py-2"
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
+
+        <button
+          className="bg-amber-700 text-white rounded-full px-6 py-2"
+          onClick={toggleDarkMode}
+        >
+          Toggle Dark Mode
+        </button>
+      </div>
+
+
+
+      <form onSubmit={handleSubmit} className="mt-8">
+      {
+        submittedName && (
+          <p className="text-2xl mt-4 ">
+            Hello, {submittedName}! Welcome to the Counter App.
+          </p>
+        )
+      }
+        <input type="text"    className="border border-gray-400 rounded px-4 py-2 mt-6" value={name} onChange={(e)=>setName(e.target.value)} />
+
+        <button type="submit"  className="bg-blue-500 text-white rounded px-6 py-2 ml-4">Submit</button>
+      </form>
+
+  
+    </div>
+  );
+};
+
+export default usestate;
+
