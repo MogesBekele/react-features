c1bd51e675ac02cfeef5ec77e42a6aa3faa0e80a
MogesBekele <mogesbekele32@gmail.com>
2025-04-09 14:09:19 +0300

    create useeffect component

---
+++ react-feature/src/component/UseEffect.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const UseEffect = () => {
+  // useEffect is a hook that allows you to perform side effects in function components.
+  const [fetchData, setFetchData] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+
+
+
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UseEffect
