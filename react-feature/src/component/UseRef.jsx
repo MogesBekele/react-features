69c765459090c1deb97de779af0fb404ef7eb09b
MogesBekele <mogesbekele32@gmail.com>
2025-04-10 16:47:35 +0300

    commmit on the useref

--- react-feature/src/component/UseRef.jsx
+++ react-feature/src/component/UseRef.jsx
@@ -7,6 +7,7 @@ const UseRef = () => {
   const editorRef = useRef(null);
 
   const handleRef = (e) => {
+    // Prevent the default form submission behavior
     e.preventDefault();
 
     const inputElement = editorRef.current;
