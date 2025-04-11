24ff41a99103a2ecfbdd1916f41fc3a64499412c
MogesBekele <mogesbekele32@gmail.com>
2025-04-11 06:04:57 +0300

    create context api

--- react-feature/src/component/Context.jsx
+++ react-feature/src/component/Context.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { createContext } from 'react'
+import React from "react";
+import { createContext } from "react";
 
-const AppContext = createContext()
-const Context = () => {
+const AppContext = createContext();
+const Context = (props) => {
+  const [count, setCount] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
 
-   const [count, setCount] = useState(0);
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    const [name, setName] = useState("");
-    const [submittedName, setSubmittedName] = useState("");
+  const value = {
+    count,
+    setCount,
+    isDarkMode,
+    setIsDarkMode,
+    name,
+    setName,
+    submittedName,
+    setSubmittedName,
+  };
   return (
     <div>
-   
+      <AppContext.Provider value={value}>{children}</AppContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default Context
+export default Context;
