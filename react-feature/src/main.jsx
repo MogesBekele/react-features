89763c8481a9576801340990bbbf35dd70502f87
MogesBekele <mogesbekele32@gmail.com>
2025-04-11 06:20:11 +0300

    commit on context api

--- react-feature/src/main.jsx
+++ react-feature/src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import AppContextProvider from './component/Context.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AppContextProvider from "./component/Context.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <AppContextProvider>
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </StrictMode>
-  </AppContextProvider>
-
-)
+);
