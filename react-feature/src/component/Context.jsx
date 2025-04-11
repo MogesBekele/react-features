01f9386fb43aaf33fc885673fb3e0b478e57915f
MogesBekele <mogesbekele32@gmail.com>
2025-04-11 06:02:43 +0300

    create context api

--- react-feature/src/component/Context.jsx
+++ react-feature/src/component/Context.jsx
@@ -3,9 +3,14 @@ import { createContext } from 'react'
 
 const AppContext = createContext()
 const Context = () => {
+
+   const [count, setCount] = useState(0);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [name, setName] = useState("");
+    const [submittedName, setSubmittedName] = useState("");
   return (
     <div>
-      
+   
     </div>
   )
 }
