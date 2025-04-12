bb063f89a95cbb1e0bcf3cbb342ed764b0c91baf
MogesBekele <mogesbekele32@gmail.com>
2025-04-12 05:56:44 +0300

    create navbar component

--- react-feature/src/App.jsx
+++ react-feature/src/App.jsx
@@ -3,6 +3,7 @@ import Usestate from './component/usestate'
 import UseEffect from './component/UseEffect'
 import UseRef from './component/UseRef'
 import Footer from './component/Footer'
+import Navbar from './component/Navbar'
 
 
 const App = () => {
