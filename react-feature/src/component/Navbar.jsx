3830468190c7f87a2ba29d44e3ed16d2f232d400
MogesBekele <mogesbekele32@gmail.com>
2025-04-12 06:14:31 +0300

    commit on the href on navbar

--- react-feature/src/component/Navbar.jsx
+++ react-feature/src/component/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-gray-800 text-white py-8 fixed top-0 w-full z-50 shadow-lg">
+      <nav  className="bg-gray-800 text-white py-8 fixed top-0 w-full z-50 shadow-lg">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-2xl font-bold">React Features</div>
+          <div className="text-2xl font-bold"><a href="#">React Features</a></div>
           <ul className="flex space-x-4">
             <li>
               <a href="#usestate" className="hover:text-blue-400">
