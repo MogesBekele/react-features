aa6266afd30222037cd981949bcdc91c7e8b9ce5
MogesBekele <mogesbekele32@gmail.com>
2025-04-12 06:25:26 +0300

    padding at the top

--- react-feature/src/component/Navbar.jsx
+++ react-feature/src/component/Navbar.jsx
@@ -3,9 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav  className="bg-gray-800 text-white py-8 fixed top-0 w-full z-50 shadow-lg">
+      <nav className="bg-gray-800 text-white py-8 fixed top-0 w-full z-50 shadow-lg">
         <div className="container mx-auto flex justify-between items-center">
-          <div ><a className="text-2xl font-bold" href="#">React Features</a></div>
+          <div>
+            <a className="text-2xl font-bold" href="#">
+              React Features
+            </a>
+          </div>
           <ul className="flex space-x-4">
             <li>
               <a href="#usestate" className="hover:text-blue-400">
